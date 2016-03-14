48ca4f8417992e511c0feea558ed34ac4ae1a193
Herve AZOULAY <hazoulay@usr.ingenico.loc>
2016-03-14 11:33:17 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var connect = require('connect');
+var stylus = require('stylus');
+
+var Transactions = require('./getTrx').Transactions;
+
+var transactions = new Transactions();
+
+var app = module.exports = express();
+var sessionTimeOut = 3600000; //1 hour
+
+var server = connect();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.limit('400mb'));
+  app.use(express.cookieParser());
+  app.use(app.router);
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(express.static(__dirname + '/public'));
+  app.use('/images/', express.static(__dirname + "/images/"));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.set('view options', { layout: true });
+app.set('view options', { pretty: true });
+app.locals.pretty = true; 
+
+app.get('/', function(req, res){
+    res.render('home', {
+            title: 'Trx Display Test'
+        });
+});
+
+app.post('/go/:pageId', function(req, res){
+    transactions.getTransactions(req.params.pageId,req.param('montantMin'), req.param('montantMax'), function(error,docs,facetList,facetPivot,total){
+        res.render('displayTrx', {
+            title: 'Transaction Journal',
+            transactions: docs,
+            facets:facetList,
+            pivots:facetPivot,
+            pageId:req.params.pageId,
+            montantMin:req.param('montantMin'),
+            montantMax:req.param('montantMax'),
+            totalRecords:total
+          });
+      });
+});
+
+app.post('/go/file/export', function(req, res) {
+transactions.export( req.param('montantMin'), req.param('montantMax'), req.param('totalRecords'), function (data) {
+        res.set({'Content-Type' :'application/octet-stream', 
+          'Content-length' : data.length , 
+          'Content-disposition' :'attachment; filename=testouille.zip'});
+        res.send(data);
+        res.on( 'data' , function(data) { console.log ('yo')});
+      });
+ });
+
+app.listen(3000);
---
+++ getTrx.js
@@ -0,0 +1,46 @@
+var ZipWriter = require("moxie-zip").ZipWriter;
+
+var nodeZip = new require('node-zip')();
+
+
+var solr = require('solr-client');
+var client = solr.createClient();
+
+Transactions = function() {
+};
+
+Transactions.prototype.getTransactions = function (page,montantMin, montantMax, callback) {
+  var query = 'q=*&fq=Montant:[' + montantMin + '%20TO%20' + montantMax + ']&facet=true&facet.field=Monnaie&facet.field=Type_d_application&facet.pivot={!stats=piv1}Monnaie,Type_de_transaction,Type_d_application&stats=true&stats.field={!tag=piv1%20sum=true}Montant&rows=30&start=' + page*50;
+  client.get('trx/select', query, function(err, obj){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(obj.response.numFound);
+      callback(null, obj.response.docs, obj.facet_counts.facet_fields, obj.facet_counts.facet_pivot,obj.response.numFound);
+  }
+});
+}
+
+Transactions.prototype.export = function (montantMin, montantMax, totalRecords, callback) {
+  var query = 'q=*&fq=Montant:[' + montantMin + '%20TO%20' + montantMax + ']&rows=' + totalRecords + "'";
+  var zip = new ZipWriter();
+  client.get('trx/select', query, function(err, obj){
+    if(err){
+      console.log(err);
+    } else {
+      var data = "";
+      for(var trx in obj.response.docs)
+      {
+        data += obj.response.docs[trx].Date_Ticket + "," + obj.response.docs[trx].Date_Serveur + "," + obj.response.docs[trx].Monnaie + "\r\n";
+      }
+      console.log( "data " + data);
+      zip.addData("results.txt", data);
+      zip.toBuffer(function(buf) {
+       callback(buf);
+    });
+ }
+});
+}  
+
+exports.Transactions = Transactions;
+
